7c0bf414e6e8bb52e068d84b7d13f8692c19f117
ArthurMarianoNeto <thiesto@gmail.com>
2021-01-27 11:13:31 -0300

    Inclusão de Figura na tela de cadastro de contato
    Incluido TextField para incluir Nome
    
    Ao incluir nome o AppBar muda com o nome instantaneamente

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:contatos/helpers/contact_helpers.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,7 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
-
+  bool _userEdited = false;
   Contact _editedContact;
 
   @override
@@ -38,8 +39,37 @@ class _ContactPageState extends State<ContactPage> {
         child: Icon(Icons.save),
         backgroundColor: Colors.lightBlueAccent,
       ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10.0) ,
+        child: Column(
+          children: [
+            GestureDetector(
+              child: Container(
+                width: 140.0, height: 140.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: _editedContact.img != null ?
+                      FileImage(File(_editedContact.img)) :
+                      AssetImage("images/person.jpg")
+                  ),
+                ),
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "Nome"
+              ),
+              onChanged: (text){
+                _userEdited = true;
+                setState(() {
+                  _editedContact.nome = text;
+                });
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
-
-
 }
